d158238792c60605cc32b7073721c824fcea420e
XSana <nico@niconi.cn>
2025-02-25 18:14:42 +0800

    feat(web-controller): 添加控制器工具提示和配置项
    - 在英文和中文语言文件中添加 web_controller 的工具提示
    - 在 ItemBlockWebController 和 Waila 数据提供器中显示控制器 ID
    - 在 NameConstants 中添加相关常量
    - 在 WebControllerConfig 中添加 useSSL 配置项

--- src/main/java/moe/takochan/takotech/constants/NameConstants.java
+++ src/main/java/moe/takochan/takotech/constants/NameConstants.java
@@ -4,6 +4,10 @@ public class NameConstants {
 
     public static final String BLOCK_WEB_CONTROLLER = "web_controller";
 
+    public static final String BLOCK_WEB_CONTROLLER_DESC = BLOCK_WEB_CONTROLLER + ".desc";
+
+    public static final String BLOCK_WEB_CONTROLLER_DESC_1 = BLOCK_WEB_CONTROLLER_DESC + ".1";
+
     public static final String ITEM_ORE_STORAGE_CELL = "ore_storage_cell";
 
     public static final String ITEM_ORE_STORAGE_CELL_DESC = ITEM_ORE_STORAGE_CELL + ".desc";
