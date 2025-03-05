45b0eefe2f7726060f0dd8871b66cbb4510afacd
XSana <nico@niconi.cn>
2025-03-06 01:36:42 +0800

    feat(takotech): 添加格雷科技工具类Mixin并调整客户端Mixin加载条件
    - 在 LateMixinLoader 中添加格雷科技工具类 Mixin
    - 格雷科技相关 Mixin 现在仅在检测到格雷科技模组时加载
    - 修复了 MetaBaseItemMixin 中的注解参数
    - 新增 MetaGeneratedToolMixin 类，但内部方法暂未实现

---
+++ src/main/java/moe/takochan/takotech/mixin/gt/MetaGeneratedToolMixin.java
@@ -0,0 +1,26 @@
+package moe.takochan.takotech.mixin.gt;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import gregtech.api.items.MetaGeneratedTool;
+
+@Mixin(MetaGeneratedTool.class)
+public abstract class MetaGeneratedToolMixin {
+
+    // @Inject(method = "getContainerItem*", at = @At("RETURN"), remap = false)
+    // public final ItemStack getContainerItem(ItemStack aStack) {
+    // }
+
+    /**
+     * @author XSana
+     * @reason 合成替换
+     */
+    // @Overwrite(remap = false)
+    // private ItemStack getContainerItem(ItemStack aStack, boolean playSound) {
+    // return new ItemStack(ItemLoader.ITEM_TOOLBOX_PLUS);
+    // }
+    //
+    // @Override
+    // public final boolean doDamageToItem(ItemStack aStack, int aVanillaDamage) {
+    // }
+}
