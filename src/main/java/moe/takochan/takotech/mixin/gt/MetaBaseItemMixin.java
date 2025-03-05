86d92fba6df90cdc1823e4c15a253738d4aa1e7a
XSana <nico@niconi.cn>
2025-03-05 23:55:49 +0800

    feat(toolbox): 高级工具箱增加切换工具提示信息
    - 在 ItemToolboxPlus 类中添加物品提示信息方法
    - 在 MetaBaseItemMixin 中为格雷科技工具添加切换工具提示信息
    - 更新英文和中文语言文件，添加新的提示信息文本

---
+++ src/main/java/moe/takochan/takotech/mixin/gt/MetaBaseItemMixin.java
@@ -0,0 +1,40 @@
+package moe.takochan.takotech.mixin.gt;
+
+import static moe.takochan.takotech.client.gui.settings.GameSettings.selectTool;
+
+import java.util.List;
+
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import gregtech.api.items.MetaBaseItem;
+import gregtech.api.items.MetaGeneratedTool;
+import moe.takochan.takotech.constants.NBTConstants;
+import moe.takochan.takotech.constants.NameConstants;
+import moe.takochan.takotech.utils.CommonUtils;
+import moe.takochan.takotech.utils.I18nUtils;
+
+@Mixin(MetaBaseItem.class)
+public abstract class MetaBaseItemMixin {
+
+    @Inject(method = "addInformation", at = @At("RETURN"), remap = false)
+    public final void addInformation(ItemStack aStack, EntityPlayer aPlayer, List<String> aList, boolean aF3_H,
+        CallbackInfo ci) {
+        if (aStack.getItem() instanceof MetaGeneratedTool) {
+            NBTTagCompound nbt = CommonUtils.openNbtData(aStack);
+            if (!nbt.hasKey(NBTConstants.TOOLBOX_DATA) || !nbt.hasKey(NBTConstants.TOOLBOX_SELECTED_INDEX)) return;
+            aList.add("");
+            aList.add(
+                I18nUtils.tooltip(
+                    NameConstants.ITEM_TOOLBOX_PLUS_DESC,
+                    GameSettings.getKeyDisplayString(selectTool.getKeyCode())));
+        }
+    }
+}
