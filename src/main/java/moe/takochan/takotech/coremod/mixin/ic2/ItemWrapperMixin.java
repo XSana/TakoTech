d6b9f1d37c53e4298e37d80c334e4d7db041fb3c
XSana <nico@niconi.cn>
2025-03-10 17:54:32 +0800

    temp: 暂存

---
+++ src/main/java/moe/takochan/takotech/coremod/mixin/ic2/ItemWrapperMixin.java
@@ -0,0 +1,26 @@
+package moe.takochan.takotech.coremod.mixin.ic2;
+
+import gregtech.api.items.MetaGeneratedTool;
+import ic2.api.item.ItemWrapper;
+import moe.takochan.takotech.constants.NBTConstants;
+import moe.takochan.takotech.utils.CommonUtils;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ItemWrapper.class, remap = false)
+public class ItemWrapperMixin {
+
+    @Inject(method = "canBeStoredInToolbox", at = @At("RETURN"), cancellable = true)
+    private static void canBeStoredInToolbox(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (stack != null) {
+            if (stack.getItem() instanceof MetaGeneratedTool) {
+                NBTTagCompound nbt = CommonUtils.openNbtData(stack);
+                if (nbt.hasKey(NBTConstants.TOOLBOX_DATA)) cir.setReturnValue(false);
+            }
+        }
+    }
+}
