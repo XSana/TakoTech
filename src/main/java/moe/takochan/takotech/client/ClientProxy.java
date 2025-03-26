c255e26b68bfc777fe4a495465fcf4e7d8f26790
XSana <nico@niconi.cn>
2025-03-26 19:50:35 +0800

    feat(client): 添加 GUI 模糊背景效果（未实现）
    - 新增了水平和垂直模糊着色器
    - 添加了 ShaderProgram 和 ShaderType 类来管理着色器

--- src/main/java/moe/takochan/takotech/client/ClientProxy.java
+++ src/main/java/moe/takochan/takotech/client/ClientProxy.java
@@ -1,6 +1,9 @@
 package moe.takochan.takotech.client;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import moe.takochan.takotech.client.renderer.shader.ShaderType;
 import moe.takochan.takotech.client.settings.GameSettings;
 import moe.takochan.takotech.common.CommonProxy;
 
@@ -10,11 +13,25 @@ public class ClientProxy extends CommonProxy {
     // Don't forget to call the super methods as well.
 
     @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+        // 注册并编译Shader
+        ShaderType.register();
+    }
+
+    @Override
     public void init(FMLInitializationEvent event) {
         super.init(event);
 
         // 创建游戏设置实例并注册按键绑定
         GameSettings gameSettings = new GameSettings();
         gameSettings.register();
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+        Runtime.getRuntime()
+            .addShutdownHook(new Thread(ShaderType::cleanupAll));
     }
 }
---
+++ src/main/java/moe/takochan/takotech/client/renderer/shader/ShaderProgram.java
@@ -0,0 +1,48 @@
+package moe.takochan.takotech.client.renderer.shader;
+
+import moe.takochan.takotech.TakoTechMod;
+import org.lwjgl.opengl.GL20;
+
+public class ShaderProgram extends com.gtnewhorizon.gtnhlib.client.renderer.shader.ShaderProgram {
+
+    public ShaderProgram(String domain, String vertShaderFilename, String fragShaderFilename) {
+        super(domain, vertShaderFilename, fragShaderFilename);
+    }
+
+    public void setUniform(String name, float value) {
+        int loc = GL20.glGetUniformLocation(this.getProgram(), name);
+        if (loc >= 0) {
+            GL20.glUniform1f(loc, value);
+        } else {
+            TakoTechMod.LOG.warn("Uniform '{}' not found in shader {}", name, this.getProgram());
+        }
+    }
+
+    public void setUniform(String name, int value) {
+        int loc = GL20.glGetUniformLocation(this.getProgram(), name);
+        if (loc >= 0) {
+            GL20.glUniform1i(loc, value);
+        } else {
+            TakoTechMod.LOG.warn("Uniform '{}' not found in shader {}", name, this.getProgram());
+        }
+    }
+
+    public void setUniform2f(String name, float x, float y) {
+        int loc = GL20.glGetUniformLocation(this.getProgram(), name);
+        if (loc >= 0) {
+            GL20.glUniform2f(loc, x, y);
+        } else {
+            TakoTechMod.LOG.warn("Uniform '{}' not found in shader {}", name, this.getProgram());
+        }
+    }
+
+    public void setUniform3f(String name, float x, float y, float z) {
+        int loc = GL20.glGetUniformLocation(this.getProgram(), name);
+        if (loc >= 0) {
+            GL20.glUniform3f(loc, x, y, z);
+        } else {
+            TakoTechMod.LOG.warn("Uniform '{}' not found in shader {}", name, this.getProgram());
+        }
+    }
+
+}
